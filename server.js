ac1cf007bc4949ccdb92a4fd1af525bcaeaad41c
unityprogramming40 <123001594+unityprogramming40@users.noreply.github.com>
2024-11-28 23:11:45 +0200

    ..

--- server.js
+++ server.js
@@ -1,21 +1,26 @@
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
+
+// Import controllers
 const GunController = require('./controllers/gunController');
 const AdminController = require('./controllers/adminController');
 const PlayerController = require('./controllers/playerController');
 const GameplayController = require('./controllers/gameplayController');
 
+// Initialize Express app and HTTP server
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
+// Initialize controllers with the Socket.IO instance
+const gunController = new GunController(io);
+const playerController = new PlayerController(io);
+const adminController = new AdminController(io, playerController);
+const gameplayController = new GameplayController(io);
 
-// Initialize Controllers with Socket.IO instance
-const GNC = new GunController(io);
-const PLC = new PlayerController(io);
-const ADC = new AdminController(io,PLC);
-const GPC = new GameplayController(io);
-
+// Start the server
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+server.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
